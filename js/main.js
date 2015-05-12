1ce1ffb3ae706280f6a26cd0bd03eda7cbe4c658
rdeco <rdecodesigns@gmail.com>
2015-05-12 11:59:49 -1000

    add js/ file

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$('#myModal1').modal({
+  show: false
+});
+
+
+$('#myModal2').modal({
+  show: false
+});
+
+$('#myModal3').modal({
+  show: false
+});
+
+
+
+(function($) {
+"use strict";
+
+	$(function() {
+
+		if( 0 < $('#post_media').length ) {
+		$('form').attr('enctype', 'multipart/form-data');
+	} // end if
+
+});
+}(jQuery));
+
+
